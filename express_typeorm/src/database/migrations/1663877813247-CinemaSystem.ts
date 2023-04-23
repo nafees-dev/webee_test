6a4f3cba8df76926e5d1b7d236c0cc99de6a8c18
nafees-dev <mn36505@gmail.com>
2023-04-23 16:00:23 +0500

    first commit

--- express_typeorm/src/database/migrations/1663877813247-CinemaSystem.ts
+++ express_typeorm/src/database/migrations/1663877813247-CinemaSystem.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CinemaSystem1663877813247 implements MigrationInterface {
   /**
@@ -31,8 +31,56 @@ export class CinemaSystem1663877813247 implements MigrationInterface {
    * As a cinema owner I dont want to configure the seating for every show
    */
   public async up(queryRunner: QueryRunner): Promise<void> {
-    throw new Error('TODO: implement migration in task 4');
+    await queryRunner.query(`CREATE TABLE movies (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR NOT NULL,
+      duration INTERVAL NOT NULL
+    )`);
+
+    await queryRunner.query(`CREATE TABLE showrooms (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR NOT NULL
+    )`);
+
+    await queryRunner.query(`CREATE TABLE shows (
+      id SERIAL PRIMARY KEY,
+      start_time TIMESTAMP NOT NULL,
+      end_time TIMESTAMP NOT NULL,
+      movie_id INTEGER NOT NULL REFERENCES movies(id),
+      showroom_id INTEGER NOT NULL REFERENCES showrooms(id),
+      price DECIMAL(5,2) NOT NULL,
+      CONSTRAINT unique_movie_showroom_start_time UNIQUE (movie_id, showroom_id, start_time)
+    )`);
+
+    await queryRunner.query(`CREATE TABLE seat_types (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR NOT NULL,
+      premium DECIMAL(5,2) NOT NULL
+    )`);
+
+    await queryRunner.query(`CREATE TABLE seats (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR NOT NULL,
+      type_id INTEGER NOT NULL REFERENCES seat_types(id),
+      showroom_id INTEGER NOT NULL REFERENCES showrooms(id),
+      CONSTRAINT unique_seat_name_showroom UNIQUE (name, showroom_id)
+    )`);
+
+    await queryRunner.query(`CREATE TABLE bookings (
+      id SERIAL PRIMARY KEY,
+      show_id INTEGER NOT NULL REFERENCES shows(id),
+      seat_id INTEGER NOT NULL REFERENCES seats(id),
+      CONSTRAINT unique_show_seat_booking UNIQUE (show_id, seat_id)
+    )`);
+    // throw new Error("TODO: implement migration in task 4");
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE bookings`);
+    await queryRunner.query(`DROP TABLE seats`);
+    await queryRunner.query(`DROP TABLE seat_types`);
+    await queryRunner.query(`DROP TABLE shows`);
+    await queryRunner.query(`DROP TABLE showrooms`);
+    await queryRunner.query(`DROP TABLE movies`);
+  }
 }
